500e1e25286bbc04945a2de5faa1aad2d1132351
Konstantin Büttner <kb@automation-monitoring.com>
2025-10-09 17:16:13 +0200

    Migrate server side call for special agent to new API

--- lib/python3/cmk_addons/plugins/agent_db/server_side_calls/agent_db.py
+++ lib/python3/cmk_addons/plugins/agent_db/server_side_calls/agent_db.py
@@ -5,17 +5,19 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # This file is part of the checkmk "Database Special Agent" agent_db (https://github.com/automation-monitoring/agent_db)
 
-# Location: ~/local/share/check_mk/checks/agent_db
-
 import json
 import base64
 from copy import deepcopy
 
-from cmk.base.check_api import passwordstore_get_cmdline
+from cmk.server_side_calls.v1 import (
+    SpecialAgentCommand,
+    SpecialAgentConfig,
+    noop_parser,
+)
 
 
-def agent_db_arguments(params, hostname, ipaddress):
-    password_arg = passwordstore_get_cmdline("%s", params["password"])
+def generate_agent_db_commands(params, host_config):
+    password_arg = params["password"].unsafe()
 
     # Build dict to later dump to b64. We need to deepcopy here,
     # because modifying params leads to errors when this function
@@ -30,8 +32,7 @@ def agent_db_arguments(params, hostname, ipaddress):
     if params_b64["db_backend"][0] == "cmk_oracle":
         oracle_params = params_b64["db_backend"][1]
         if "asm_credentials" in oracle_params:
-            asm_pw = passwordstore_get_cmdline("%s",
-                        oracle_params["asm_credentials"]["asm_password"])
+            asm_pw = oracle_params["asm_credentials"]["asm_password"].unsafe()
             del oracle_params["asm_credentials"]["asm_password"]
 
     # Dump params to JSON and serialize to a base64 string
@@ -42,15 +43,19 @@ def agent_db_arguments(params, hostname, ipaddress):
         "--base64args",
         base64_params,
         "--hostname",
-        hostname,
+        host_config.name,
         "--ipaddress",
-        ipaddress,
+        host_config.ipv4_config.address,
         "--password",
         password_arg,
     ]
     if asm_pw is not None:
         args += ["--asm_password", asm_pw]
-    return args
+    yield SpecialAgentCommand(command_arguments=args)
 
 
-special_agent_info["db"] = agent_db_arguments
+special_agent_db = SpecialAgentConfig(
+    name="db",
+    parameter_parser=noop_parser,
+    commands_function=generate_agent_db_commands,
+)
